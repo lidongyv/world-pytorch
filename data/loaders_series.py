2da8e209af8265bad15fcf218480383dcd776fca
pobingwanghai <pobingwanghai@gmail.com>
2019-05-24 18:10:32 +0800

    series

---
+++ data/loaders_series.py
@@ -0,0 +1,182 @@
+""" Some data loading utilities """
+from bisect import bisect
+from os import listdir
+from os.path import join, isdir
+from tqdm import tqdm
+import torch
+import torch.utils.data
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+N=5000
+M=1000
+O=1000
+class _RolloutDataset(torch.utils.data.Dataset): # pylint: disable=too-few-public-methods
+    def __init__(self, root, transform, train=True): # pylint: disable=too-many-arguments
+        self._transform = transform
+        self._root=root
+        self._files = listdir(root)
+        self._files.sort()
+        if train:
+            self._files = self._files[:N]
+            self._data = self._create_dataset(self._files, N, M)
+        else:
+            self._files = self._files[-O:]
+            self._data = self._create_dataset(self._files, O, M)
+
+        self._length=self.__len__()
+
+
+    def __len__(self):
+        # to have a full sequence, you need self.seq_len + 1 elements, as
+        # you must produce both an seq_len obs and seq_len next_obs sequences
+        # if not self._cum_size:
+        #     self.load_next_buffer()
+        return len(self._data)
+
+    def _create_dataset(self,filelist, N=10000, M=1000):  # N is 10000 episodes, M is number of timesteps
+        #data = np.zeros((M * N, 64, 64, 3), dtype=np.uint8)
+        data=[]
+        idx = 0
+        for i in range(N):
+            count=0
+            filename = filelist[i]
+            raw_data = np.load(os.path.join(self._root, filename))['obs']
+            l = len(raw_data)
+            true_data=[]
+            # check_data=np.sum(raw_data,dim=-1)
+            for j in range(l):
+                if np.max(raw_data[j])<255:
+                    data.append((raw_data[j]))
+            # raw_data=np.stack(true_data)
+            l=len(data)
+            if i%100==0:
+                print("loading file", i + 1,"having length",l)
+        data=np.stack(data)
+            # if (idx + l) > (M * N):
+            #     data = data[0:]
+            #     print('premature break')
+            #     break
+            # if i<12000:
+            #     count=0
+            #     # for j in range(l):
+            #     #     if np.mean(data[j])>10:
+            #     #         print(i,j,np.mean(data[j]))
+            #     #         count+=1
+            #     #         # plt.figure("Image")  # 图像窗口名称
+            #     #         # plt.imshow(data[j])
+            #     #         # plt.axis('on')  # 关掉坐标轴为 off
+            #     #         # plt.title('image')  # 图像题目
+            #     #         # plt.show()
+            #     #         # break
+            #     # print(count)
+            #     # count=0
+            #     for j in range(l):
+            #         if np.mean(data[j])<1:
+            #             #print(i,j,np.mean(data[j]))
+            #             count+=1
+            #             if j%500==0:
+            #                 plt.figure("Image")  # 图像窗口名称
+            #                 plt.imshow(data[j])
+            #                 plt.axis('on')  # 关掉坐标轴为 off
+            #                 plt.title('image')  # 图像题目
+            #                 plt.show()
+            #                 print(os.path.join(self._root, filename))
+            #             #break
+            #     if count>0:
+            #         print(i,count)
+            # else:
+            #     exit()
+            # data[idx:idx + l] = raw_data
+            # idx += l
+
+            # if ((i + 1) % 100 == 0):
+            #     print("loading file", i + 1)
+        return data
+    def __getitem__(self, index):
+        data = self._data[index]
+        return self._get_data(data)
+
+    def _get_data(self, data):
+        pass
+
+    def _data_per_sequence(self, data_length):
+        pass
+
+
+class RolloutSequenceDataset(_RolloutDataset): # pylint: disable=too-few-public-methods
+    """ Encapsulates rollouts.
+
+    Rollouts should be stored in subdirs of the root directory, in the form of npz files,
+    each containing a dictionary with the keys:
+        - observations: (rollout_len, *obs_shape)
+        - actions: (rollout_len, action_size)
+        - rewards: (rollout_len,)
+        - terminals: (rollout_len,), boolean
+
+     As the dataset is too big to be entirely stored in rams, only chunks of it
+     are stored, consisting of a constant number of files (determined by the
+     buffer_size parameter).  Once built, buffers must be loaded with the
+     load_next_buffer method.
+
+    Data are then provided in the form of tuples (obs, action, reward, terminal, next_obs):
+    - obs: (seq_len, *obs_shape)
+    - actions: (seq_len, action_size)
+    - reward: (seq_len,)
+    - terminal: (seq_len,) boolean
+    - next_obs: (seq_len, *obs_shape)
+
+    NOTE: seq_len < rollout_len in moste use cases
+
+    :args root: root directory of data sequences
+    :args seq_len: number of timesteps extracted from each rollout
+    :args transform: transformation of the observations
+    :args train: if True, train data, else test
+    """
+    def __init__(self, root, seq_len, transform,  train=True): # pylint: disable=too-many-arguments
+        super().__init__(root, transform, train)
+        self._seq_len = seq_len
+
+    def _get_data(self, data, seq_index):
+        obs_data = data['observations'][seq_index:seq_index + self._seq_len + 1]
+        obs_data = self._transform(obs_data.astype(np.float32))
+        obs, next_obs = obs_data[:-1], obs_data[1:]
+        action = data['actions'][seq_index+1:seq_index + self._seq_len + 1]
+        action = action.astype(np.float32)
+        reward, terminal = [data[key][seq_index+1:
+                                      seq_index + self._seq_len + 1].astype(np.float32)
+                            for key in ('rewards', 'terminals')]
+        # data is given in the form
+        # (obs, action, reward, terminal, next_obs)
+        return obs, action, reward, terminal, next_obs
+
+    def _data_per_sequence(self, data_length):
+        return data_length - self._seq_len
+
+class RolloutObservationDataset(_RolloutDataset): # pylint: disable=too-few-public-methods
+    """ Encapsulates rollouts.
+
+    Rollouts should be stored in subdirs of the root directory, in the form of npz files,
+    each containing a dictionary with the keys:
+        - observations: (rollout_len, *obs_shape)
+        - actions: (rollout_len, action_size)
+        - rewards: (rollout_len,)
+        - terminals: (rollout_len,), boolean
+
+     As the dataset is too big to be entirely stored in rams, only chunks of it
+     are stored, consisting of a constant number of files (determined by the
+     buffer_size parameter).  Once built, buffers must be loaded with the
+     load_next_buffer method.
+
+    Data are then provided in the form of images
+
+    :args root: root directory of data sequences
+    :args seq_len: number of timesteps extracted from each rollout
+    :args transform: transformation of the observations
+    :args train: if True, train data, else test
+    """
+    def _data_per_sequence(self, data_length):
+        return data_length
+
+    def _get_data(self, data):
+        return self._transform(data)
--- series.py
+++ series.py
@@ -69,79 +69,16 @@ trained=0
 model=VAE(3, LSIZE)
 model=torch.nn.DataParallel(model,device_ids=range(8))
 model.cuda()
-optimizer = optim.Adam(model.parameters(),lr=learning_rate,betas=(0.9,0.999))
-# scheduler = ReduceLROnPlateau(optimizer, 'min', factor=0.5, patience=5)
-# earlystopping = EarlyStopping('min', patience=30)
-
-vis = visdom.Visdom(env='old_tf_data')
-
-ground_window = vis.image(
-    np.random.rand(64, 64),
-    opts=dict(title='ground!', caption='ground.'),
-)
-image_window = vis.image(
-    np.random.rand(64, 64),
-    opts=dict(title='image!', caption='image.'),
-)
-loss_window = vis.line(X=torch.zeros((1,)).cpu(),
-                       Y=torch.zeros((1)).cpu(),
-                       opts=dict(xlabel='minibatches',
-                                 ylabel='Loss',
-                                 title='Training Loss',
-                                 legend=['Loss']))
-# Reconstruction + KL divergence losses summed over all elements and batch
-def loss_function(recon_x, x, mu, logsigma):
-    """ VAE loss function """
-    #BCE = torch.mean(torch.sum(torch.pow(recon_x-x,2),dim=(1,2,3)))
-    BCE = F.mse_loss(recon_x,x,reduction='sum')/recon_x.shape[0]
-
-    # see Appendix B from VAE paper:
-    # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
-    # https://arxiv.org/abs/1312.6114
-    # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
-    KLD = -0.5 * torch.sum(1 + logsigma - torch.pow(mu,2) - torch.exp(logsigma),dim=1)
-    #KLD=torch.max(KLD,torch.ones_like(KLD).cuda()*LSIZE*0.5)
-    KLD=torch.mean(KLD)
-    # print(KLD.shape,logsigma.shape,mu.shape)
-    # exit()
-    print(BCE.item(),KLD.item())
-    return BCE + KLD
 
 
 def train(epoch):
     """ One training epoch """
-    model.train()
+    model.test()
     train_loss = []
-    for batch_idx, data in enumerate(train_loader):
-        data = data.cuda()
-        # print(torch.max(data))
-        # exit()
-        optimizer.zero_grad()
-        recon_batch, mu, logvar = model(data)
-        loss = loss_function(recon_batch, data, mu, logvar)
-        loss.backward()
-        #print(loss.item())
-        train_loss.append(loss.item())
-        optimizer.step()
-        ground = data[0,...].data.cpu().numpy().astype('float32')
-        ground = np.reshape(ground, [3,64, 64])
-        vis.image(
-            ground,
-            opts=dict(title='ground!', caption='ground.'),
-            win=ground_window,
-        )
-        image = recon_batch[0,...].data.cpu().numpy().astype('float32')
-        image = np.reshape(image, [3, 64, 64])
-        vis.image(
-            image,
-            opts=dict(title='image!', caption='image.'),
-            win=image_window,
-        )
-        vis.line(
-            X=torch.ones(1).cpu() * batch_idx + torch.ones(1).cpu() * (epoch - trained-1)* args.batch_size,
-            Y=loss.item() * torch.ones(1).cpu(),
-            win=loss_window,
-            update='append')
+    for batch_idx, data,actions in enumerate(train_loader):
+        with torch.no_grad():
+            data = data.cuda()
+            recon_batch, mu, logvar = model(data)
         if batch_idx % 1 == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
@@ -150,34 +87,6 @@ def train(epoch):
 
     print('====> Epoch: {} Average loss: {:.4f}'.format(
         epoch, np.mean(train_loss) ))
-
-
-def test():
-    """ One test epoch """
-    model.eval()
-    test_loss = []
-    with torch.no_grad():
-        for batch_idx, data in enumerate(train_loader):
-            data = data.cuda()
-            recon_batch, mu, logvar = model(data)
-            test_loss.append(loss_function(recon_batch, data, mu, logvar).item())
-            ground = data[0, ...].data.cpu().numpy().astype('float32')
-            ground = np.reshape(ground, [3, 64, 64])
-            vis.image(
-                ground,
-                opts=dict(title='ground!', caption='ground.'),
-                win=ground_window,
-            )
-            image = recon_batch[0,...].data.cpu().numpy().astype('float32')
-            image = np.reshape(image, [3, 64, 64])
-            vis.image(
-                image,
-                opts=dict(title='image!', caption='image.'),
-                win=image_window,
-            )
-    test_loss =np.mean(test_loss)
-    print('====> Test set loss: {:.4f}'.format(test_loss))
-    return test_loss
 
 # check vae dir exists, if not, create it
 vae_dir = join(args.logdir, 'vae')
@@ -193,7 +102,6 @@ if not args.noreload and exists(reload_file):
               state['epoch'],
               state['precision']))
     model.load_state_dict(state['state_dict'])
-    optimizer.load_state_dict(state['optimizer'])
     trained=state['epoch']
     #trained=0
     # scheduler.load_state_dict(state['scheduler'])
